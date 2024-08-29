9749e8a5abcfc10d076af95c5cf68602095e98a2
Mariana Morais <141177767+moraismariana@users.noreply.github.com>
2024-08-29 15:02:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+// Altura da seção inicial
+function ajusteTamanho() {
+  const sectionInicial = document.querySelector("#inicio div");
+  sectionInicial.style.setProperty(
+    "height",
+    `${window.innerHeight}px`,
+    "important"
+  );
+}
+
+function verificarLargura() {
+  if (window.innerWidth <= 768) {
+    ajusteTamanho();
+    window.addEventListener("resize", ajusteTamanho);
+  }
+}
+
+window.addEventListener("load", verificarLargura);
+
+// SLIDER
+let screenWidth = window.innerWidth;
+const splideOptions = {
+  autoplay: true,
+  arrows: false,
+  drag: false,
+  pauseOnHover: false,
+
+  interval: 5000,
+  speed: 1100,
+  focus: "center",
+  slideFocus: true,
+  type: "loop",
+  fixedWidth: "60vw",
+  fixedHeight: "70vh",
+  gap: "12px",
+
+  breakpoints: {
+    1300: {
+      fixedWidth: "70vw",
+      fixedHeight: "80vh",
+    },
+    768: {
+      fixedWidth: screenWidth,
+      fixedHeight: "70vh",
+      drag: true,
+      arrows: false,
+      gap: "12px",
+    },
+    480: {
+      fixedHeight: "50vh",
+    },
+  },
+};
+
+new Splide(".splide", splideOptions).mount();
+
+// MENU MOBILE
+function menuShow() {
+  let menuMobile = document.querySelector(".mobile-menu");
+  let header = document.querySelector("header");
+  menuMobile.classList.toggle("none");
+  header.classList.toggle("active");
+}
+
+document.querySelectorAll(".mobile-menu a").forEach((link) => {
+  link.addEventListener("click", function () {
+    menuShow();
+  });
+});
